41f4fb78aa47e5438c03bae94682d4877092f51a
Romain Menke <romainmenke@gmail.com>
2026-02-18 08:39:06 +0100

    add support for eslint 10

---
+++ test/test-eslint-9.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const rule = require("../lib/rule.js");
+const RuleTester = require("eslint-9").RuleTester;
+
+const ruleTester = new RuleTester();
+
+ruleTester.run("uncalled-iife", rule, {
+	valid: [
+		{
+			code: "function A() { }"
+		},
+		{
+			code: "(function(){ }())"
+		},
+		{
+			code: "(function(){ })()"
+		},
+		{
+			code: "var alpha = function(){ };"
+		}
+	],
+	invalid: [
+		{
+			code: "(function(){ })",
+			errors: [{ message: "Possible uncalled IIFE" }]
+		},
+		{
+			code: "(function(){ });(function(){ }())",
+			errors: [{ message: "Possible uncalled IIFE" }]
+		}
+	]
+});
